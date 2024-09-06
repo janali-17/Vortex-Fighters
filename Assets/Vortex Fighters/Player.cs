201f60653333156411f10a9b8edf901072d71f9f
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-09-06 08:48:51 -0700

    player can jump now (only one jump per tap)

--- Assets/Vortex Fighters/Player.cs
+++ Assets/Vortex Fighters/Player.cs
@@ -7,10 +7,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 10.0f;
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    [SerializeField]
+    private bool Grounded;
+
+    //Handles
+    private Rigidbody _rigidbody;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -20,7 +27,29 @@ public class Player : MonoBehaviour
 
     private void Movement()
     {
-     transform.Translate (Input.GetAxis("Horizontal")*Time.deltaTime,0.0f,0.0f * _speed);
-     transform.Translate (0.0f,0.0f, Input.GetAxis("Vertical") * _speed * Time.deltaTime);
+     transform.Translate (Input.GetAxis("Horizontal")*Time.deltaTime * _speed, 0.0f,Input.GetAxis("Vertical") * Time.deltaTime * _speed);
+
+        if(Input.GetKeyDown(KeyCode.Space) && Grounded == true)
+        {
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x,_jumpForce,_rigidbody.velocity.z);
+        }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("On ground");
+            Grounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("jumping");
+            Grounded = false;
+        }   
+    }
+
 }
