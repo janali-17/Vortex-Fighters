fdf016637b2781b793c4808f7b3448e73ec18b26
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-09-06 08:18:11 -0700

    Player movement set with (AWSD) keys

--- Assets/Vortex Fighters/Player.cs
+++ Assets/Vortex Fighters/Player.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+     transform.Translate (Input.GetAxis("Horizontal")*Time.deltaTime,0.0f,0.0f * _speed);
+     transform.Translate (0.0f,0.0f, Input.GetAxis("Vertical") * _speed * Time.deltaTime);
     }
 }
